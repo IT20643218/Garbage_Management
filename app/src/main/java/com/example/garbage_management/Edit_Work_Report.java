bc66999e1707be4bbe1344b8a5c0addbff4cf71b
Sumedha Lakmal Bandaranayaka <it20643218@my.sliit.lk>
2022-05-02 14:15:03 +0530

    Work Report Crud

---
+++ app/src/main/java/com/example/garbage_management/Edit_Work_Report.java
@@ -0,0 +1,14 @@
+package com.example.garbage_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_Work_Report extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_work_report);
+    }
+}
---
+++ app/src/main/java/com/example/garbage_management/Work_Reports.java
@@ -0,0 +1,14 @@
+package com.example.garbage_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Work_Reports extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_reports);
+    }
+}
