3e9f51144178638e3144b2d9cd3a4f22a98f8152
Sumedha Lakmal Bandaranayaka <it20643218@my.sliit.lk>
2022-05-05 12:41:24 +0530

    Work Report Crud

--- app/src/main/java/com/example/garbage_management/Home.java
+++ app/src/main/java/com/example/garbage_management/Home.java
@@ -2,13 +2,40 @@ package com.example.garbage_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Home extends AppCompatActivity {
+    EditText name,password;
+    Button login,register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        name = findViewById(R.id.homename);
+        password = findViewById(R.id.homepassword);
+        login = findViewById(R.id.homelogin);
+        register = findViewById(R.id.homeregister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),W_Repo.class);
+                startActivity(i);
+            }
+        });
+
+        /*register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(getApplicationContext(),Work_Reports.class);
+                startActivity(a);
+            }
+        });*/
     }
 }
